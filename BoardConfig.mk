ef58359a5e960b0b80657ac617a1fd2db258cf16
Coconut <coconutat@protonmail.com>
2024-08-09 11:13:18 +0800

    stanford: set Partitions - dynamic in here
    move it from hi3660 common tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,15 @@ DEVICE_PATH := device/huawei/stanford
 # Inherit from hi3660-common
 include device/huawei/hi3660-common/BoardConfigCommon.mk
 
+# Partitions - dynamic
+BOARD_SUPER_PARTITION_BLOCK_DEVICES := cust eng_system eng_vendor odm patch preas preavs prets pretvs product system vendor
+BOARD_SUPER_PARTITION_PRETS_DEVICE_SIZE := 25165824
+BOARD_SUPER_PARTITION_PRETVS_DEVICE_SIZE := 8388608
+BOARD_SUPER_PARTITION_PRODUCT_DEVICE_SIZE := 763363328
+BOARD_SUPER_PARTITION_SYSTEM_DEVICE_SIZE := 2894069760
+
+BOARD_SUPER_PARTITION_SIZE := 6299844608
+
 # Display
 TARGET_SCREEN_DENSITY := 420
 
